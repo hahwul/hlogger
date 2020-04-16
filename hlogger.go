85ddff94a3ff38fd7cb6c4eeeba2d39535c4c134
hahwul <hahwul@gmail.com>
2020-04-17 01:20:45 +0900

    update

---
+++ hlogger.go
@@ -0,0 +1,32 @@
+package hlogger
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/logrusorgru/aurora"
+)
+
+var (
+	mutex = &sync.Mutex{}
+)
+
+func log(level, text string, args ...interface{}) {
+	if level == "INFO" {
+		text = aurora.Blue("[INFO] ").String() + text
+	}
+	if level == "WEAK" {
+		text = aurora.Yellow("[WEAK] ").String() + text
+	}
+	if level == "VULN" {
+		text = aurora.Red("[VULN] ").String() + text
+	}
+	if level == "SYSTEM" {
+		text = aurora.White("[*] ").String() + text
+	}
+	mutex.Lock()
+	fmt.Fprintf(os.Stderr, text)
+	mutex.Unlock()
+
+}
